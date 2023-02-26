30c99985da7ba41219af128863b23eb79eacf726
darknbright <126418985+darknbright@users.noreply.github.com>
2023-02-26 21:37:42 +0100

    Initial commit

---
+++ index.1.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html <?php language_attributes();?>>
+<head>
+    <meta charset="<?php bloginfo('charset');?>">
+    <?php wp_head(); ?>
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>db</title>
+    <style>
+        :lang(ar) {
+            direction: rtl;
+        }
+    </style>-->
+</head>
+<body <?php body_class('example'); ?>>
+    <?php wp_body_open(); ?>
+
+     <p>Ciao</p>
+    <?php wp_footer(); ?>
+</body>
+</html>
+
